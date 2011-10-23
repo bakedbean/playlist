e8e04bc98eff0f1be30137f6d3a3acca487b1f5d
Eben Goodman <eben@ebenimac.local>
2011-10-23 18:39:57 -0400

    added file extension regex check

--- app/models/tune.rb
+++ app/models/tune.rb
@@ -3,7 +3,11 @@ class Tune < ActiveRecord::Base
   
   has_many  :rating, :dependent => :destroy
   
-  validates :url,       :presence => true
+  url_regex = /^.*\.(mp3|MP3|m4a|M4A|mp4|MP4)$/i
+  
+  validates :url,       :presence => true,
+                        :format     => { :with => url_regex, :message => "Please specify a valid audio file format" }
+                        
   validates :title,     :presence => true
   validates :duration,  :numericality => { :only_integer => true }
   
